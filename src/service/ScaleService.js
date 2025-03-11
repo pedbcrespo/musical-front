53d7959a532fde126fa0ae6bb17daa1866fe3487
pedro <pedbcrespo@gmail.com>
2025-03-11 12:15:48 -0300

    tirando o localhost

--- src/service/ScaleService.js
+++ src/service/ScaleService.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export default class ScaleService {
     constructor() {
-        // this.url = 'https://pbcrespo.pythonanywhere.com';
-        this.url = 'http://127.0.0.1:5000';
+        this.url = 'https://pbcrespo.pythonanywhere.com';
     } 
 
     getNotes() {
